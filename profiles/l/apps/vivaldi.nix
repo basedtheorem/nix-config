1758ee491cdc86402a66dc3194574546ed4f848f
basedtheorem <lrns@proton.me>
2023-09-04 23:10:18 +1200

    fix(vivaldi): broken startup due to gpu flags

--- profiles/l/apps/vivaldi.nix
+++ profiles/l/apps/vivaldi.nix
@@ -2,20 +2,22 @@
   programs.vivaldi = {
     enable = true;
 
-    # commandLineArgs = [
+    commandLineArgs = [
       # "--enable-gpu-rasterization"
       # "--enable-oop-rasterization"
       # "--enable-gpu-compositing"
       # "--enable-accelerated-2d-canvas"
-      # "--enable-zero-copy"
+      "--enable-zero-copy"
       # "--enable-unsafe-webgpu"
+      "--ignore-gpu-blocklist"
+      "--use-gl=desktop"
       # "--use-vulkan"
       # "--canvas-oop-rasterization"
       # "--disable-features=UseChromeOSDirectVideoDecoder,UseSkiaRenderer"
       # "--enable-accelerated-video-decode"
       # "--enable-accelerated-video-encode"
-      # "--enable-features=VaapiVideoDecoder,VaapiVideoEncoder,VaapiIgnoreDriverChecks,VaapiVideoDecodeLinuxGL"
+      "--enable-features=VaapiVideoDecoder"
       # "--enable-hardware-overlays"
-    # ];
+    ];
   };
 }
