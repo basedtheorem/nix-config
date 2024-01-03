2b3749611568d7641964497b7bb9f9533d543539
1rns <lrns@proton.me>
2024-01-03 21:06:00 +1300

    feat: package micro-autofmt

--- packages/default.nix
+++ packages/default.nix
@@ -11,6 +11,7 @@
     packages = {
       v-shell = pkgs.callPackage ./v-shell.nix {};
       swhkd = pkgs.callPackage ./swhkd.nix {};
+      micro-autofmt = pkgs.callPackage ./micro-autofmt.nix {source = inputs.micro-autofmt;};
     };
   };
 }
