e8c65a340062d296cc582ddb11073c2a6e8b56c4
1rns <lrns@proton.me>
2024-02-16 05:01:49 +1300

    feat: package clock-face

---
+++ packages/clock-face.nix
@@ -0,0 +1,35 @@
+{ lib, stdenvNoCC, fetchzip }:
+
+stdenvNoCC.mkDerivation rec {
+  pname = "ClockFace-font";
+  version = "bad1107";
+
+  # ClockFace, ClockFaceSolid, ClockFaceRect, ClockFaceRectSolid
+  src = fetchzip {
+    # Use the 'ttc' files here for a smaller closure size.
+    # (Using 'ttf' files gives a closure size about 15x larger, as of November 2021.)
+    url =
+      "https://github.com/ocodo/ClockFace-font/archive/bad11070c962d328679e9bfec7769fb920097615.zip";
+    hash = "sha256-2+kiG5apYMKI5P1o1ahr9BsV87UVWjSmM8S+Vf5MAO0=";
+  };
+
+  sourceRoot = ".";
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/share/fonts/truetype
+    cd $src
+    cp *.ttf $out/share/fonts/truetype
+
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "Icon font for displaying the time";
+    homepage = "https://github.com/ocodo/ClockFace-font";
+    license = licenses.cc-by-nc-30;
+    maintainers = [ maintainers."1rns" ];
+    platforms = platforms.all;
+  };
+}
--- packages/default.nix
+++ packages/default.nix
@@ -1,11 +1,15 @@
-{ ... }: {
+{ inputs, ... }: {
   _file = ./default.nix;
 
-  perSystem = { system, pkgs, lib, inputs', ... }: {
+  perSystem = { system, pkgs, ... }: {
     packages = {
       v-shell = pkgs.callPackage ./v-shell.nix { };
       swhkd = pkgs.callPackage ./swhkd.nix { };
       lentenrose = pkgs.callPackage ./lentenrose.nix { };
+      clock-face = (import inputs.master {
+        inherit system;
+        config.allowUnfree = true;
+      }).callPackage ./clock-face.nix { };
     };
   };
 }
